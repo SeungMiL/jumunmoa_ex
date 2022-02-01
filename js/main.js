fcff9e269b5ba35f680c42f154da0aa65c5b4a33
SeungMiL <tmskqj@naver.com>
2022-02-02 00:06:58 +0900

    jumunmoa_2

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 
 
-$('.list').click(function(e){ 
+$('.list').click(function (e) {
     openTab(e.target.dataset.id)
-  });
+});
 
-  $(".tab_button").click(function(e){ 
-        openTab(e.target.dataset.id)
-        $(location).attr("href", "#")
-    })
+$(".tab_button").click(function (e) {
+    openTab(e.target.dataset.id)
+    $(location).attr("href", "#")
+})
 
 
 
-  function openTab(i){
-    $('.tab_button').removeClass('active'); 
-    $('.tab_content').removeClass('show'); 
-    $('.tab_button').eq(i).addClass('active'); 
-    $('.tab_content').eq(i).addClass('show'); 
-  }
+function openTab(i) {
+    $('.tab_button').removeClass('active');
+    $('.tab_content').removeClass('show');
+    $('.tab_button').eq(i).addClass('active');
+    $('.tab_content').eq(i).addClass('show');
+}
 
